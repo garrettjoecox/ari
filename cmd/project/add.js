1c9ee499f00a2641ad0f565d6317b8f660c2d866
joelcoxokc <joelcox@hisimagination.com>
2015-04-04 03:01:54 -0700

    Update: clean(project)

--- cmd/project/add.js
+++ cmd/project/add.js
@@ -1,73 +1,76 @@
-var
 /*
-    Dependencies
+ *  @action    ADD
+ *
+ *  @command   PROJECT
  */
 
-      path = require('path')
-    , extend = require('lodash/object/extend')
-    , gulpTemplate = require('gulp-template')
-    , rename     = require('gulp-rename')
-    , gulp       = require('gulp')
-    , logger     = require('../../lib/logger')
-    , Err        = logger.Err
-    , Ok         = logger.Ok
-    , templates  = path.join.bind(path, path.join(__dirname, '../../templates'))
-    , settings   = require('./settings/add')
-    , ask        = require('inquirer').prompt
-    , prompts    = require('./prompts/add')
+'use strict'
 
+/*
+ *  Dependencies
+ */
+var path = require('path')
+    , templates    = path.join.bind(path, path.join(__dirname, '../../templates'))
+    , settings     = require('./settings/add')
+    , prompts      = require('./prompts/add')
+    , logger       = require('../../lib/logger')
+    , extend       = require('lodash/object/extend')
+    , ask          = require('inquirer').prompt
+    , Err          = logger.Err
+    , Ok           = logger.Ok
 ;
 module.exports = function(config, project, action, target, name){
 
-    var
     /*
      *  Locals
      *  @source    the final source path for gulp to grab
      *  @sourceDir source dir path for source
      *  @dest      the destination for gulp.dest
      */
-          source
-        , filter
-        , sourceDir = templates(target)
-        , dest      = path.join(config.root, (project.path || '/projects/'+project.name), 'src', name);
+    var source
+      , dest      = path.join(config.root, (project.path || '/projects/'+project.name), 'src', name)
+      , filter
+      , task      = require('./tasks/add')
+      , sourceDir = templates(target)
     ;
 
     settings  = settings(target, name)
     source    = sourceDir + settings.glob.all
     filter    = settings.filter
 
-    ////////////////////////////////
-
     filter.name  = name
     filter.names = settings.names
 
-    ask(prompts(target), function(answers){
-        extend(filter, answers)
-        start()
-    });
+    ask(prompts(target), start);
 
-    function start(){
-        gulp.start('add');
+    /*
+        START
+     */
+    function start(answers){
+        extend(filter, answers)
+        task(source, dest, filter, onErr, onEnd).start('add')
     }
 
-    gulp.task('add', function(done){
-        gulp.src(source)
-            .pipe(gulpTemplate(filter))
-            .pipe(rename(function(file){
-                if (file.basename === '_') {
-                    file.basename = settings.names.kebab
-                }
-                return file;
-            }))
-            .pipe(gulp.dest(dest))
-            .on('error', function(){
-                Err('Issue adding ${a}', name)
-            })
-            .on('end', function(){
-                done();
-                Ok('Finished creating ${a} ${b}', target, name)
-                process.exit(0)
-            })
-    })
 
+    /*
+        ON ERROR
+     */
+    function onErr(cb) {
+        return function(){
+            cb()
+            Err('Issue adding ${a}', filter.name)
+            process.exit(0)
+        }
+    }
+
+    /*
+        ON END
+     */
+    function onEnd(cb) {
+        return function(){
+            cb();
+            Ok('Finished creating ${a} ${b}', target, filter.name)
+            process.exit(0)
+        }
+    }
 }
--- cmd/project/settings/add.js
+++ cmd/project/settings/add.js
@@ -1,62 +1,66 @@
+/*
+ *  @settings  ADD
+ *
+ *  @action    ADD
+ *  @command   PROJECT
+ */
+'use strict'
+
 var extend = require('lodash/object/extend')
   , kebabCase  = require('lodash/string/kebabCase')
   , capitalize = require('lodash/string/capitalize')
   , camelCase = require('lodash/string/camelCase')
-module.exports = (function(){
-    var
+;
 
-    /*
-        Exports
-     */
-      defaults  = {
-          name       : ''
-        , imports    : []
-        , exporting  : true
-        , inherits   : false
-        , getters    : []
-        , setters    : []
-        , prototypes : []
-        , params     : []
-      }
-    , filters = {
-          class   : defaults
-        , element : {
-              imports    : [{name:'Behavior', from:'aurelia-templating'}]
-            , metadata   : []
-            , inject     : ['Element']
-            , params     : ['element']
-            , view       : true
-          }
-        , attribute : {
-              imports    : [{name:'Behavior', from:'aurelia-templating'}]
-            , metadata   : []
-            , inject     : ['Element']
-            , params     : ['element']
-            , view       : false
-          }
-        , resource : {
-            index: true
-          }
-      }
-    , globs = {
-          class: {
-            all: '**/*'
-          }
-        , element : {
-              model: '/model/*'
-            , view : '/view/*'
-            , all  : '/**/*'
-          }
+var defaults = {
+
+      name       : ''
+    , imports    : []
+    , exporting  : true
+    , inherits   : false
+    , getters    : []
+    , setters    : []
+    , prototypes : []
+    , params     : []
+}
+
+var filters = {
+
+    class   : defaults,
+    element : {
+          imports    : [{name:'Behavior', from:'aurelia-templating'}]
+        , metadata   : []
+        , inject     : ['Element']
+        , params     : ['element']
+        , view       : true
+    },
+    attribute : {
+          imports    : [{name:'Behavior', from:'aurelia-templating'}]
+        , metadata   : []
+        , inject     : ['Element']
+        , params     : ['element']
+        , view       : false
+    },
+    resource : {
+        index: true
     }
-    return function (target, name) {
-      return {
+}
+
+var globs = {
+
+  class: {
+      all: '**/*'
+  },
+  element : {
+      model: '/model/*'
+    , view : '/view/*'
+    , all  : '/**/*'
+  }
+}
+module.exports = function(target, name){
+    return {
           filter  : extend(defaults, filters[target])
         , glob    : globs[target]
-        , names   : {
-            kebab : kebabCase(name),
-            class : capitalize(camelCase(name))
-          }
-      };
-    };
-
-})()
+        , names   : { kebab:kebabCase(name), class:capitalize(camelCase(name)) }
+    }
+}
