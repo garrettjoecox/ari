1727db3e9a43598f1edd955012a78665ce280364
garrettjoecox <garrettjcox@gmail.com>
2015-04-04 01:27:10 -0700

    Fixed Issue(error on empty projects section)

--- bin/ari.js
+++ bin/ari.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
-// Slice arguments into an array
-process.ARI = {};
+/* Main 'ari' routing command */
+
 var args = process.argv.slice(2);
 
-// If there are no arguments run help
 if (!args.length){
     require('../cmd/help')();
     return;
@@ -14,7 +13,6 @@ var fs = require('fs');
 var Err = require('../lib/logger').Err;
 var configPath = process.cwd() + '/.ari-config.json';
 
-// Checks for configuration file
 if (!fs.existsSync(configPath)){
     Err('Config not found');
     Err('Please run ari init');
@@ -23,14 +21,12 @@ if (!fs.existsSync(configPath)){
 
 var config = fs.readFileSync(configPath);
 
-// Checks if config is empty for some reason?
 if (!config.length){
     Err('Config file is empty');
     Err('Please run ari init');
     return;
 }
 
-// Parses config for use
 config = JSON.parse(config);
 var commands = {
     init: true,
@@ -41,16 +37,15 @@ var commands = {
     project: true,
 }
 
-// If first argument is the name of a project in the config, add 'project' to the front of the args array
+config.projects = config.projects || {};
 if (config.projects[args[0]]) args.unshift('project');
 
+process.ARI = {};
 process.ARI.args = args;
 process.ARI.config = config;
 process.ARI.configPath = configPath;
 
-// If the command exists require that file and run it
 if (commands[args[0]]) require('../cmd/' + args[0])();
-// Else run help
 else {
     require('../cmd/help')();
     Err('Command not found');
