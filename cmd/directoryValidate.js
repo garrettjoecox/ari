ea571f80e12539df35490c6ff62b97ff47400014
garrettjoecox <garrettjcox@gmail.com>
2015-04-05 17:44:00 -0700

    fix spacing(changed to 4)

--- cmd/directoryValidate.js
+++ cmd/directoryValidate.js
@@ -4,14 +4,14 @@ var fs = require('fs');
 
 module.exports = function(){
 
-  if (!fs.existsSync(process.cwd() + '/.ari-config.json')) {
-    Ari.err();
-    Ari.err("You're not in an ari directory!");
-    Ari.err('Try running ari init');
-    Ari.err();
-    process.exit(0);
-  }
+    if (!fs.existsSync(process.cwd() + '/.ari-config.json')) {
+        Ari.err();
+        Ari.err("You're not in an ari directory!");
+        Ari.err('Try running ari init');
+        Ari.err();
+        process.exit(0);
+    }
 
-  Ari.config = require(process.cwd() + '/.ari-config.json');
+    Ari.config = require(process.cwd() + '/.ari-config.json');
 
 };
--- cmd/help.js
+++ cmd/help.js
@@ -2,12 +2,12 @@
 var Ari = process.ARI;
 
 module.exports = function(){
-  Ari.log('');
-  Ari.log('Welcome to Ari');
-  Ari.log('');
-  Ari.log('Ari Init: ');
-  Ari.log('Ari Add: ');
-  Ari.log('Ari Delete: ');
-  Ari.log('');
-  process.exit(0);
+    Ari.log('');
+    Ari.log('Welcome to Ari');
+    Ari.log('');
+    Ari.log('Ari Init: ');
+    Ari.log('Ari Add: ');
+    Ari.log('Ari Delete: ');
+    Ari.log('');
+    process.exit(0);
 };
